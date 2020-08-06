84d434857faeb232604a12e9b1010b1d89c3d1ff
habibyeo2 <habibyeo2@gmail.com>
2020-08-06 01:27:09 -0400

    first coomit

---
+++ app.js
@@ -0,0 +1,50 @@
+const tea4TeamFCC = getTea(40);
+
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+
+return fixedValue + 1;
+
+function incrementer(value) {
+    return value + 1;}
+
+var ratings = watchList.map(item => ({
+ title: item["Title"],
+rating: item["imdbRating"]
+}));
+
+this.forEach(a => newArray.push(callback(a)));
+
+var filteredList = watchList
+  .map(movie => {
+    return {
+      title: movie.Title,
+      rating: movie.imdbRating
+    };
+  })
+  .filter(movie => {
+    return parseFloat(movie.rating) >= 8.0;
+  });
+
+//
+
+  let newArray = [];
+  this.forEach(function(x) {
+    if (callback(x) == true) {
+      newArray.push(x);
+    }
+  });
+//
+
+
+  let newArray = [];
+  this.forEach(function(x) {
+    if (callback(x) == true) {
+      newArray.push(x);
+    }
+  });
+//
+
+return anim.slice(beginSlice, endSlice);
+
+return cities.slice(0, 3);
